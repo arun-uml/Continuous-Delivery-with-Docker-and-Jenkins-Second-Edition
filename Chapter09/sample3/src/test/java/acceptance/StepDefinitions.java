faddf3712f5bd9a55f6e4afc8f7d32528537f1eb
arun-uml <124535265+arun-uml@users.noreply.github.com>
2023-03-21 01:07:42 -0400

    Update StepDefinitions.java

--- Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
+++ Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
@@ -35,6 +35,12 @@ public class StepDefinitions {
         assertEquals(expectedResult, result);
     }
     
+    @Given("^I have dividend and divisor: (.*) and (.*)$")
+    public void i_have_dividend_and_divisor(String a, String b) throws Throwable {
+        this.a = a;
+        this.b = b;
+    }
+    
     @When("^the calculator divides them$")
     public void the_calculator_divides_them() throws Throwable {
         String url = String.format("%s/div?a=%s&b=%s", server, a, b);
